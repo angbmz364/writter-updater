ecae78a895318e88525a7c028aab05b9b54ea7cd
sask <angbmz364@gmail.com>
2026-04-21 00:49:20 -0500

    🔧 improve styles & add background selector

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,26 @@
 const output = document.getElementById("output");
 const input = document.getElementById("input");
 const container = document.getElementById("output-container");
+const bodyClassList = document.body.classList
+
+const buttonWhite = document.getElementById("white-bg");
+const buttonGreen = document.getElementById("green-bg");
+
+buttonWhite.addEventListener('click', ()=> {
+    buttonGreen.classList.remove("focus");
+    buttonWhite.classList.add("focus");
+    document.body.classList.add("white");
+    document.body.classList.remove("green");
+}
+)
+buttonGreen.addEventListener('click', ()=> {
+    buttonGreen.classList.add("focus");
+    buttonWhite.classList.remove("focus");
+    document.body.classList.remove("white");
+    document.body.classList.add("green");
+}
+)
+
 
 function fitText() {
     let fontSize = 90; 
